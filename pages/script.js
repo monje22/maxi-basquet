c55b314b650a0ea95315745a120fe00295d594a7
Ciden-23 <67012953+Ciden-23@users.noreply.github.com>
2022-10-16 22:18:20 -0400

    Script de conexion con la BD V2
    Script de registro de usuarios, inicio de sesion y listener de sesiones activas
    No verifica campos ni nada (Aun)
    Documentacion del codigo realizada
    Pequeñas modificaciones realizadas en html para utilizar el script

--- pages/script.js
+++ pages/script.js
@@ -8,10 +8,40 @@ const firebaseConfig = {
     measurementId: "G-GVRPKCYGBB"
 };
 
+
+
+/**
+ * @constant app Conexion a la API de Firebase
+ * @constant auth Base de datos encargada unicamente de los usuarios (correo y contraseña)
+ * @var db Base de datos que almacena cualquier tipo de informacion(texto) en colecciones
+ */
 const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 var db = firebase.firestore();
 
+
+/**
+ * Listener de los usuarios, recupera la sesion activa(si existe)
+ * o redirecciona al login
+ * @param user El objeto usuario de la sesion activa
+ */
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        var uid = user.uid;
+        console.log(user)
+            // ...
+    } else {
+        // User is signed out
+        // ...
+    }
+});
+
+
+/**
+ * Funcion que realiza el registro de los usuarios en la BD
+ */
 function registrarUser() {
     let correo = document.getElementById("1").value;;
     let contraseña = document.getElementById("2").value;;
@@ -19,39 +49,73 @@ function registrarUser() {
     let apellido = document.getElementById("4").value;;
     let fechaN = document.querySelector('input[type="date"]');
     let nomcomp = nombre + " " + apellido;
+    /*Funcion propia de Firebase que realiza el registro de usuarios
+    Esta funcion unicamente acepta 2 parametros: correo y contraseña
+    */
     auth.createUserWithEmailAndPassword(correo, contraseña)
         .then((userCredential) => {
-            // Signed in
+            // Inicio de sesion automatico
             const user = userCredential.user;
             let uid = user.uid;
+            // (Firebase) Llamada a la funcion updateProfile para añadir el nombre completo a la cuenta
             user.updateProfile({
                 displayName: nomcomp,
                 photoURL: null
             }).then(() => {
-                // Update successful
+                // Actualizacion exitosa
                 console.log(user.displayName)
-                    // ...
+                    // Creacion de una constante que almacenara el nombre y la fecha de nacimiento
                 const initialData = {
                     Nombre: nomcomp,
                     fechaNac: fechaN.value
                 };
+                //(Firestore) Funcion que añade una nueva coleccion de datos a la BD
                 db.collection('userData').doc('user').collection(uid).doc('datos iniciales').set(initialData);
             }).catch((error) => {
-                // An error occurred
+                // Ocurrio un error al registrar los datos del usuario
                 var errorCode = error.code;
                 var errorMessage = error.message;
                 alert(msg(errorCode))
-                    // ...
+
             });
         })
         .catch((error) => {
+            // Ocurrio un error al registrar al usuario
             var errorCode = error.code;
             var errorMessage = error.message;
             alert(msg(errorCode))
-                // ..
         });
 }
 
+
+/**
+ * Funcion que realiza el inicio de sesion de los usuarios
+ */
+function loginUser() {
+    let correo = document.getElementById("1").value;;
+    let contraseña = document.getElementById("2").value;;
+    auth.signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Signed in
+            var user = userCredential.user;
+            console.log(user)
+                // ...
+        })
+        .catch((error) => {
+            // Ocurrio un error al inicar sesion
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            alert(msg(errorCode))
+        });
+
+}
+
+
+/**
+ * Funcion que envia codigos de error en español(Firebase los manda en ingles)
+ * @param {String} errorCode Codigo de error personalizado o enviado por Firebase
+ * @returns Codigo de error en español o personalizado
+ */
 function msg(errorCode) {
     let msg;
     switch (errorCode) {
