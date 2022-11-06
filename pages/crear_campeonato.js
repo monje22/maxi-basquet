8cb003b170fc4511be1a63f74cff3a3fe926cec3
antonio monje <antoniomonje2@gmail.com>
2022-11-05 21:47:12 -0400

    validaciones crear campeonato v2

--- pages/crear_campeonato.js
+++ pages/crear_campeonato.js
@@ -1,14 +1,37 @@
+
 formulario = document.querySelectorAll('#formulario');
 const inputs = document.querySelectorAll('#formulario input');
 const form = document.getElementById("formulario");
 const Logo_campeonato = document.getElementById("File_logo");
 const QR = document.getElementById("QR-file");
+//INPUTS_DATES
+const fechaInicio = document.getElementById("Fecha_ini");
+const fechaFin = document.getElementById("Fecha_fin");
+const fechaInscripcion = document.getElementById("Fecha_Inicio_inscription");
+const fechaLimiteInsc = document.getElementById("Fecha_limite_incription");
+
+const campos={
+    Nombre : false ,
+    Categoria:false,
+    Rama:false,
+    Invitacion : false,
+    NombreO:false,
+    FechaInicio:false,
+    FechaFin:false,
+    FechaInscripcion:false,
+    FechaLimiteInsc:false,
+    CostoPre:false,
+    Costo:false,
+    ImagenQr:false
+};
 
 const expresiones ={
-    Nombre: /^[a-zA-Z ñ Ñ]{2,70}$/,
-    invitacion:/^[a-zA-ZñÑ " "]{10-1000}$/,
-    Organizador: /^[a-zA-Z ñ Ñ]{2,70}$/,
-    Costo:/^[0-9]$/
+    Nombre: /^[a-zA-Z ñ Ñ]{5,70}$/,
+    invitacion:/^[\w ñÑ?¿=,.;'"-:<>]{10,1000}$/,
+    NombreO: /^[a-zA-Z ñ Ñ]{2,70}$/,
+    CostoPre:/^\d*(\.\d+)$/,
+    Costo:/^\d*(\.\d+)$/,
+    
 };
 
 const validarFormulario = (e) => {
@@ -17,17 +40,17 @@ const validarFormulario = (e) => {
         case "Nombre":
 			validarCampo(expresiones.Nombre, e.target, 'Nombre');
 		break;
-		case "Nacionalidad":
-			validarCampo(expresiones.nacionalidad, e.target, 'Nacionalidad');
+		case "Invitacion":
+			validarCampo(expresiones.invitacion, e.target, 'Invitacion');
 		break;
-		case "Edad":
-			validarCampo(expresiones.edad, e.target, 'Edad');
+		case "NombreO":
+			validarCampo(expresiones.NombreO, e.target, 'NombreO');
 		break;
-		case "Peso":
-			validarCampo(expresiones.peso, e.target, 'Peso');
+		case "CostoPre":
+			validarCampo(expresiones.CostoPre, e.target, 'CostoPre');
 		break;
-		case "Altura":
-			validarCampo(expresiones.altura,e.target,'Altura')
+		case "Costo":
+			validarCampo(expresiones.Costo,e.target,'Costo')
 		break;
 		
 	}
@@ -36,35 +59,117 @@ const validarFormulario = (e) => {
 const validarCampo = (expresion, input, campo) => {
 	if(expresion.test(input.value)){
 		
-		document.getElementById(`error-${campo}`).classList.remove('input-error-activo');
-        document.getElementById(`error-${campo}`).classList.add('input-error');
+		document.getElementById(`error-${campo}`).classList.remove('msg-error-activo');
+        document.getElementById(`error-${campo}`).classList.add('msg-error');
         document.getElementById(`${campo}`).classList.remove('is-invalid');
         document.getElementById(`${campo}`).classList.add('is-valid');
 		campos[campo] = true;
 	} else {
-		document.getElementById(`error-${campo}`).classList.add('input-error-activo');
+		document.getElementById(`error-${campo}`).classList.add('msg-error-activo');
         document.getElementById(`${campo}`).classList.add('is-invalid');
 		campos[campo] = false;
 	}
 };
 
 inputs.forEach((input) => {
-    input.addEventListener('keyup',()=>{
-       validarFormulario();
-        console.log("oa")
-    });
-    input.addEventListener('blur',()=>{
-        console.log("tuki")
-    });
+    input.addEventListener('keyup', validarFormulario );
+    input.addEventListener('blur', validarFormulario );
 });
+
+
+function errorActivo(campito){
+    
+    document.getElementById(`error-${campito}`).classList.add('msg-error-activo');
+	campos[campito] = false;
+	
+}
+
+function errorDesactivo(campito){
+    document.getElementById(`error-${campito}`).classList.remove('msg-error-activo');
+    document.getElementById(`error-${campito}`).classList.add('msg-error');
+	campos[campito] = true;
+}
+
+function Fecha(){
+    let date=new Date();
+    let day= date.getDate();
+    let month=date.getMonth() +1;
+    const year = date.getFullYear();
+    
+    if(day < 10){
+        day ="0" + day;
+    }
+    if(month<10){
+        month = "0"+ month;
+    }
+    let fecha_hoy= year + "-" + month + "-" + day;
+    return fecha_hoy;
+}
+//limpiar errores de dates
+fechaInicio.addEventListener("change", ()=>{
+    errorDesactivo('FechaInicio');
+});
+fechaFin.addEventListener("change", ()=>{
+    errorDesactivo('FechaFin');
+});
+fechaInscripcion.addEventListener("change", ()=>{
+    errorDesactivo('fechaInscripcion');
+});
+fechaLimiteInsc.addEventListener("change", ()=>{
+    errorDesactivo('fechaLimiteInsc');
+});
+    
 
 form.addEventListener("submit",e=>{
     e.preventDefault();
-    // if(campos.Nombre && campos.Altura && campos.Edad && campos.Nacionalidad && campos.Peso && campos.perfil && campos.Cedula){
-    //     console.log("subir");
-    // }
+    //verificacion dates vacios
+//fecha inicio
+    if(fechaInicio.value.length > 0){
+        console.log("hay valor");
+    }else{
+        errorActivo('FechaInicio');
+        console.log(fechaInicio.value + "hola 1");
+        console.log("no hay valoh");
+    }
+//fecha fin
+    if(fechaFin.value.length > 0){
+            campos.FechaFin = true;
+    }else{
+        errorActivo('FechaFin');
+    }
+//fecha inicio inscripcion
+    if(fechaInscripcion.value.length > 0){
+            
+    }else{
+        errorActivo('fechaInscripcion');
+    }
+//fecha limite inscripcion
+    if(fechaLimiteInsc.value.length > 0){
+            
+    }else{
+        errorActivo('fechaLimiteInsc');
+    }
+
+
+
+    if(campos.Categoria && campos.Costo && campos.CostoPre && campos.FechaFin && campos.FechaInicio && campos.FechaInscripcion && campos.FechaLimiteInsc && campos.ImagenQr && campos.Invitacion && campos.Nombre && campos.NombreO){
+        console.log("subir");
+    }
 });
 
+//Configuracion de input date
+document.addEventListener("DOMContentLoaded",()=>{
+    document.getElementById("Fecha_ini").setAttribute('min',Fecha()) ;
+});
+document.addEventListener("DOMContentLoaded",()=>{
+    document.getElementById("Fecha_fin").setAttribute('min',Fecha()) ;
+});
+document.addEventListener("DOMContentLoaded",()=>{
+    document.getElementById("Fecha_Inicio_inscription").setAttribute('min',Fecha()) ;
+});
+document.addEventListener("DOMContentLoaded",()=>{
+    document.getElementById("Fecha_limite_incription").setAttribute('min',Fecha()) ;
+});
 //previsualizaciones de imagenes
 //Logo
 document.addEventListener("DOMContentLoaded", ()=>{
